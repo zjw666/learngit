5a494de1c8c7461e08f151c197465234c1a2c712
zjw <591342608@qq.com>
2017-08-20 11:26:30 +0800

    second

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,13 @@
+$(function(){
+	$('ul.nav-tabs~article').hide();
+	$('#Welcome').show();
+});
 $('ul.nav-tabs li').click(function(){
 	$('ul.nav-tabs li').removeClass("active");
 	$(this).addClass("active");
 	$('ul.nav-tabs li a').css("color","gray");
 	$('ul.nav-tabs li.active a').css("color","black");
+	$('ul.nav-tabs~article').hide();
+	var ID = '#' + $(this).attr("name");
+	$(ID).show();
 });
