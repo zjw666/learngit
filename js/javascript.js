70cb11f906e61001f0b546eba2690a141ed62a54
zjw <591342608@qq.com>
2017-08-21 11:28:39 +0800

    介绍页及affix导航栏

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,11 @@
 $(function(){
-	$('ul.nav-tabs~article').hide();
-	$('#Welcome').show();
-	$("[data-toggle='tooltip']").tooltip();
+	$('ul.nav-tabs~article').hide();    //隐藏全部标签内容
+	$('#Welcome').show();               //使标签首页内容显示
+	$("[data-toggle='tooltip']").tooltip();  //激活提示工具
+	$("ul.nav-pills.nav-stacked.affix").width($("#scroll").width());  //使affix导航栏宽度动态变化
 });
-$('ul.nav-tabs li').click(function(){
+
+$('ul.nav-tabs li').click(function(){            //标签页之间内容转换
 	$('ul.nav-tabs li').removeClass("active");
 	$(this).addClass("active");
 	$('ul.nav-tabs li a').css("color","gray");
@@ -12,3 +14,16 @@ $('ul.nav-tabs li').click(function(){
 	var ID = '#' + $(this).attr("name");
 	$(ID).show();
 });
+
+$('a').focus(function(){        //去掉所有链接获得焦点时的虚线外边框
+	$(this).blur();
+});
+
+$(window).resize(function(){   //使affix导航栏宽度动态变化
+	$("ul.nav-pills.nav-stacked.affix").width($("#scroll").width());
+});
+
+$(window).scroll(function(){   //使affix导航栏宽度动态变化
+	$("ul.nav-pills.nav-stacked.affix").width($("#scroll").width());
+});
+
