77cd0021b7caf13780ed35d4aff303e41989ee8e
zjw <591342608@qq.com>
2017-11-07 21:13:18 +0800

    清除导航按钮visited样式，修正表单提交失败时评论渲染错误

--- zjw-web/web/comments/views.py
+++ zjw-web/web/comments/views.py
@@ -9,7 +9,7 @@ def contact(request):
 			form.save()
 			return redirect('/contact?response=True')
 		else:
-			comment_list = Comment.objects.all().order_by('-created_time')
+			comment_list = Comment.objects.all().filter(allow = True).order_by('-created_time')
 			context = {'form':form,'comment_list':comment_list}
 			return render(request,'contact.html',context=context)
 	return redirect('/contact/')		
